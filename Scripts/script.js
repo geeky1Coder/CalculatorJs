a96ce15aef11b7f43f422f2516215dbcb77884aa
Yuvraj Mann <yuvrajmann282@gmail.com>
2020-05-30 14:57:28 +0530

    Fixed problems happening when someone presses equals button simuntaneously

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,9 @@ var firstExp=0;
 var operationCall=false;
 var equalsCall=false;
 var preOperator;
+var e_flag=0;
+var oper;
+var oper_num;
 function insert(num){
     var s=document.querySelector(".lower-panel");
       if(operationCall==true){
@@ -16,9 +19,22 @@ function insert(num){
    }
     s.innerHTML+=num;
 }
+
+function reverseNum(num) {
+	return (
+    parseFloat(
+      num
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(num)
+  )
+}
 function operation(operator){
        
         var exp=0;
+       
         exp=document.querySelector(".lower-panel").textContent;
         exp=Number(exp);
          if(operator=="+"){
@@ -170,27 +186,80 @@ function operation(operator){
             preOperator=operator;
             operationCall=true;
         }
-
+     
         else if(operator=="="){
-          var s=document.querySelector(".upper-panel").textContent;
-          if(preOperator=="+"){
-            var result=firstExp+Number(document.querySelector(".lower-panel").textContent);
-          }
-          else if(preOperator=="*"){
-            var result=firstExp*Number(document.querySelector(".lower-panel").textContent);
-          }
-          else if(preOperator=="-"){
-            var result=firstExp-Number(document.querySelector(".lower-panel").textContent);
-          }
-          else if(preOperator=="/"){
-            var result=firstExp/Number(document.querySelector(".lower-panel").textContent);
-          }
-        
-          document.querySelector(".upper-panel").textContent+=preOperator+document.querySelector(".lower-panel").textContent+"=";
-          document.querySelector(".lower-panel").textContent=result;
-        
+          
+          if(equalsCall==true)
+             {
+               console.log(e_flag);
+               
+                if(e_flag==0){
+                  var u=document.querySelector(".upper-panel");
+                  var l=document.querySelector(".lower-panel");
+                  var n=u.textContent.length-2;
+                  var s="";
+              
+                  while(isNaN(u.textContent.charAt(n))==false)
+                  { 
+                    s+=(u.textContent).charAt(n);
+                    --n;
+                  }
+                  var o=u.textContent.charAt(n);
+                  oper=o;
+                  s=Number(s);
+                  s=reverseNum(s);
+                  oper_num=s;
+                  // firstExp=s;
+                  u.textContent="";
+                  e_flag=1;
+                }
+                console.log(oper_num);
+                console.log(oper);
+                var str;
+                str=document.querySelector(".lower-panel").textContent;
+                firstExp=Number(str);
+               
+                if(oper=="+"){
+                  
+                  document.querySelector(".upper-panel").textContent=firstExp+"+"+oper_num+"=";
+                  firstExp+=Number(oper_num);
+                }
+                else if(oper=="-"){
+                 
+                  document.querySelector(".upper-panel").textContent=firstExp+"-"+oper_num+"=";
+                  firstExp-=Number(oper_num);
+                }
+                else if(oper=="*"){
+                 
+                  document.querySelector(".upper-panel").textContent=firstExp+"*"+oper_num+"=";
+                  firstExp-=Number(oper_num);
+                }
+                else if(oper=="/"){
+                 
+                  document.querySelector(".upper-panel").textContent=firstExp+"*"+oper_num+"=";
+                  firstExp-=Number(oper_num);
+                }
+                document.querySelector(".lower-panel").textContent=firstExp;
+                  // u.textContent=l.textContent+"+"+s;
+                  // firstExp+=Number(l.textContent);
+             }
+             else{
+              if(preOperator=="+"){
+                var result=firstExp+Number(document.querySelector(".lower-panel").textContent);
+              }
+              else if(preOperator=="*"){
+                var result=firstExp*Number(document.querySelector(".lower-panel").textContent);
+              }
+              else if(preOperator=="-"){
+                var result=firstExp-Number(document.querySelector(".lower-panel").textContent);
+              }
+              else if(preOperator=="/"){
+                var result=firstExp/Number(document.querySelector(".lower-panel").textContent);
+              }
+            
+              document.querySelector(".upper-panel").textContent+=preOperator+document.querySelector(".lower-panel").textContent+"=";
+              document.querySelector(".lower-panel").textContent=result;
+             }
           equalsCall=true;
         }
-        
-        console.log(preOperator);
  }
